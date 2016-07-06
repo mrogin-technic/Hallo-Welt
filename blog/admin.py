ae694b5d754f6ffb88a4059fd133c3c85927d8ed
mrogin-technic <m.rogin.technic@gmail.com>
2016-07-06 17:27:15 +0200

    v010 Erweitert v1

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import Post
-# superuser ist: admin / pass1234
 
 admin.site.register(Post)
--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# superuser ist: admin / pass1234
+# user: mrogin / Technic2016
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
