80d77293f4f85a5366475800b1e32451daa2f384
mrogin-technic <m.rogin.technic@gmail.com>
2016-07-07 16:47:32 +0200

    v016 final2

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'blog',
 ]
 
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls_tut',
+        'USER': 'maik',
+        'PASSWORD': 'mrogin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
